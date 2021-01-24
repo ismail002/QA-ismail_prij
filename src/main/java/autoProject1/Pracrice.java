0c5fd9d53210a6925716da752a2744d2b3a553d7
ismail002 <55784423+ismail002@users.noreply.github.com>
2021-01-24 17:13:43 -0600

    Update Pracrice.java
    some update form remot master

--- src/main/java/autoProject1/Pracrice.java
+++ src/main/java/autoProject1/Pracrice.java
@@ -3,6 +3,8 @@ package autoProject1;
 public class Pracrice {
     public static void main(String[] args) {
         System.out.println("first code");
+        
+        //ismail adjissi
     }
 
 }
